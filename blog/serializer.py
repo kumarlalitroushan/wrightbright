839d529454d736408499d67b43d61af5e63626f5
Kumar Lalit Roushan <kumarlalit@Kumars-MacBook-Air.local>
2025-07-05 18:00:22 +0530

    added serializer, permission and url path

---
+++ blog/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Blog
+
+# with the use of serializer class we can generate a json response for the blog model in the API
+class BlogSerializer(serializers.ModelSerializer):
+
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
